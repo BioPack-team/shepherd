3f40952f4c9aec040fce81dc3910dd2a8826d609
Max Wang <max@covar.com>
2025-03-18 16:25:26 -0400

    Initial implementation of shepherd v1

---
+++ aragorn/worker.py
@@ -0,0 +1,166 @@
+"""Aragorn ARA module."""
+import asyncio
+import copy
+import json
+import logging
+from pathlib import Path
+from string import Template
+import time
+from shepherd_utils.broker import get_ara_task, mark_task_as_complete
+from shepherd_utils.logger import QueryLogger, setup_logging
+from shepherd_utils.db import get_message, initialize_db
+
+setup_logging()
+
+
+def examine_query(message):
+    """Decides whether the input is an infer. Returns the grouping node"""
+    # Currently, we support:
+    # queries that are any shape with all lookup edges
+    # OR
+    # A 1-hop infer query.
+    # OR
+    # Pathfinder query
+    try:
+        # this can still fail if the input looks like e.g.:
+        #  "query_graph": None
+        qedges = message.get("message", {}).get("query_graph", {}).get("edges", {})
+    except:
+        qedges = {}
+    n_infer_edges = 0
+    for edge_id in qedges:
+        if qedges.get(edge_id, {}).get("knowledge_type", "lookup") == "inferred":
+            n_infer_edges += 1
+    pathfinder = n_infer_edges == 3
+    if n_infer_edges > 1 and n_infer_edges and not pathfinder:
+        raise Exception("Only a single infer edge is supported", 400)
+    if (n_infer_edges > 0) and (n_infer_edges < len(qedges)):
+        raise Exception("Mixed infer and lookup queries not supported", 400)
+    infer = n_infer_edges == 1
+    if not infer:
+        return infer, None, None, pathfinder
+    qnodes = message.get("message", {}).get("query_graph", {}).get("nodes", {})
+    question_node = None
+    answer_node = None
+    for qnode_id, qnode in qnodes.items():
+        if qnode.get("ids", None) is None:
+            answer_node = qnode_id
+        else:
+            question_node = qnode_id
+    if answer_node is None:
+        raise Exception("Both nodes of creative edge pinned", 400)
+    if question_node is None:
+        raise Exception("No nodes of creative edge pinned", 400)
+    return infer, question_node, answer_node, pathfinder
+
+
+async def aragorn(task, logger):
+    # given a task, get the message from the db
+    message = await get_message(task[1]["query_id"])
+    try:
+        infer, question_qnode, answer_qnode, pathfinder = examine_query(message)
+    except Exception as e:
+        print(e)
+        return None, 500
+    queries = expand_aragorn_query(message)
+    await asyncio.sleep(60)
+    await mark_task_as_complete("aragorn", task[0], logger)
+    logger.info(f"Finished task {task[0]}")
+
+
+def get_infer_parameters(input_message):
+    """Given an infer input message, return the parameters needed to run the infer.
+    input_id: the curie of the input node
+    predicate: the predicate of the inferred edge
+    qualifiers: the qualifiers of the inferred edge
+    source: the query node id of the source node
+    target: the query node id of the target node
+    source_input: True if the source node is the input node, False if the target node is the input node"""
+    for edge_id, edge in input_message["message"]["query_graph"]["edges"].items():
+        source = edge["subject"]
+        target = edge["object"]
+        query_edge = edge_id
+        predicate = edge["predicates"][0]
+        qc = edge.get("qualifier_constraints", [])
+        if len(qc) == 0:
+            qualifiers = {}
+        else:
+            qualifiers = {"qualifier_constraints": qc}
+    if ("ids" in input_message["message"]["query_graph"]["nodes"][source]) \
+            and (input_message["message"]["query_graph"]["nodes"][source]["ids"] is not None):
+        input_id = input_message["message"]["query_graph"]["nodes"][source]["ids"][0]
+        source_input = True
+    else:
+        input_id = input_message["message"]["query_graph"]["nodes"][target]["ids"][0]
+        source_input = False
+    #key = get_key(predicate, qualifiers)
+    return input_id, predicate, qualifiers, source, source_input, target, query_edge
+
+
+def get_rule_key(predicate, qualifiers):
+    keydict = {'predicate': predicate}
+    keydict.update(qualifiers)
+    return json.dumps(keydict,sort_keys=True)
+
+
+def expand_aragorn_query(input_message):
+    # Contract: 
+    # 1. there is a single edge in the query graph 
+    # 2. The edge is marked inferred.
+    # 3. Either the source or the target has IDs, but not both.
+    # 4. The number of ids on the query node is 1.
+    input_id, predicate, qualifiers, source, source_input, target, qedge_id = get_infer_parameters(input_message)
+    key = get_rule_key(predicate, qualifiers)
+    #We want to run the non-inferred version of the query as well
+    qg = copy.deepcopy(input_message["message"]["query_graph"])
+    for eid, edge in qg["edges"].items():
+        del edge["knowledge_type"]
+    with open(Path(__file__).parent / "rules_with_types_cleaned_finalized.json", "r") as file:
+        AMIE_EXPANSIONS = json.load(file)
+    messages = [{"message": {"query_graph":qg}, "parameters": input_message.get("parameters") or {}}]
+    #If we don't have any AMIE expansions, this will just generate the direct query
+    for rule_def in AMIE_EXPANSIONS.get(key,[]):
+        query_template = Template(json.dumps(rule_def["template"]))
+        #need to do a bit of surgery depending on what the input is.
+        if source_input:
+            qs = query_template.substitute(source=source,target=target,source_id = input_id, target_id='')
+        else:
+            qs = query_template.substitute(source=source, target=target, target_id=input_id, source_id='')
+        query = json.loads(qs)
+        if source_input:
+            del query["query_graph"]["nodes"][target]["ids"]
+        else:
+            del query["query_graph"]["nodes"][source]["ids"]
+        message = {"message": query, "parameters": input_message.get("parameters") or {}}
+        if "log_level" in input_message:
+            message["log_level"] = input_message["log_level"]
+        messages.append(message)
+    return messages
+
+
+async def poll_redis():
+    """Continually monitor the ara queue for tasks."""
+    # Set up logger
+    level_number = logging._nameToLevel["INFO"]
+    log_handler = QueryLogger().log_handler
+    logger = logging.getLogger("shepherd.aragorn_worker")
+    logger.setLevel(level_number)
+    logger.addHandler(log_handler)
+    # initialize opens the db connection
+    await initialize_db()
+    # continuously poll the broker for new tasks
+    while True:
+        # logger.info("trying to get aragorn tasks")
+        # get a new task for the given target
+        ara_task = await get_ara_task("aragorn", logger)
+        if ara_task is not None:
+            logger.info(f"Doing task {ara_task}")
+            # send the task to a async background task
+            # this could be async, multi-threaded, etc.
+            asyncio.create_task(aragorn(ara_task, logger))
+        else:
+            await asyncio.sleep(5)
+
+
+if __name__ == "__main__":
+    asyncio.run(poll_redis())
