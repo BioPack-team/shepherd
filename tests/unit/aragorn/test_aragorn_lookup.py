869b0c1b4a41adb6ad71df42a05e59cfa80ef97d
Max Wang <max@covar.com>
2025-06-20 21:29:18 -0400

    Add more tests for aragorn functions

---
+++ tests/unit/aragorn/test_aragorn_lookup.py
@@ -0,0 +1,48 @@
+import copy
+import json
+import logging
+
+import pytest
+
+from shepherd_utils.broker import get_task
+from tests.helpers.generate_messages import creative_query
+from workers.aragorn_lookup.worker import aragorn_lookup
+
+
+@pytest.mark.asyncio
+async def test_aragorn_creative_lookup(redis_mock, mocker):
+    """Test that Aragorn Lookup sends and gets back all the queries."""
+    mock_callback_response = mocker.patch("workers.aragorn_lookup.worker.get_message")
+    mock_callback_response.return_value = copy.deepcopy(creative_query)
+    mocker.patch("workers.aragorn_lookup.worker.save_message")
+    mocker.patch("workers.aragorn_lookup.worker.add_callback_id")
+    mock_running_callbacks = mocker.patch(
+        "workers.aragorn_lookup.worker.get_running_callbacks"
+    )
+    mock_running_callbacks.return_value = []
+    mock_response = mocker.Mock()
+    mocker.patch("httpx.AsyncClient.post", return_value=mock_response)
+    logger = logging.getLogger(__name__)
+
+    await aragorn_lookup(
+        [
+            "test",
+            {
+                "query_id": "test",
+                "workflow": json.dumps(
+                    [{"id": "aragorn.lookup"}, {"id": "aragorn.score"}]
+                ),
+            },
+        ],
+        logger,
+    )
+
+    # Get the task that the ara should have put on the queue
+    task = await get_task("aragorn.score", "consumer", "test", logger)
+    assert task is not None
+    workflow = json.loads(task[1]["workflow"])
+    # make sure the workflow was correctly passed
+    assert len(workflow) == 1
+    assert [
+        "aragorn.score",
+    ] == [op["id"] for op in workflow]
