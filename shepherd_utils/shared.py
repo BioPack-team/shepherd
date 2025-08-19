6e4105bfd243e90e1e065c949cf3e90cdfd92746
Max Wang <max@covar.com>
2025-08-19 10:11:59 -0400

    Add query log level to task for other workers

--- shepherd_utils/shared.py
+++ shepherd_utils/shared.py
@@ -89,6 +89,7 @@ async def wrap_up_task(
             "query_id": task[1]["query_id"],
             "response_id": task[1]["response_id"],
             "workflow": json.dumps(workflow),
+            "log_level": task[1]["log_level"],
             "otel": task[1]["otel"],
         },
         logger,
