5e3e4cf33f4690bce008bd2682e2bd3d12a283dc
Max Wang <max@covar.com>
2025-09-08 12:52:31 -0400

    Add in working result retrieval

--- workers/sipr/worker.py
+++ workers/sipr/worker.py
@@ -1,11 +1,20 @@
 """SIPR (Set-Input Page Rank) module."""
 
 import asyncio
+import json
 import logging
 import time
 import uuid
 
-from shepherd_utils.db import get_message
+import httpx
+
+from shepherd_utils.config import settings
+from shepherd_utils.db import (
+    add_callback_id,
+    get_message,
+    get_running_callbacks,
+    save_message,
+)
 from shepherd_utils.otel import setup_tracer
 from shepherd_utils.shared import get_tasks, wrap_up_task
 
@@ -14,16 +23,20 @@ STREAM = "sipr"
 GROUP = "consumer"
 CONSUMER = str(uuid.uuid4())[:8]
 TASK_LIMIT = 100
+MAX_QUERY_TIME = 300
 tracer = setup_tracer(STREAM)
 
 
 async def sipr(task, logger: logging.Logger):
     start = time.time()
-    workflow = []
+    workflow = [
+        {"id": "sipr"},
+    ]
     try:
         # given a task, get the message from the db
         logger.info("Getting message from db")
-        message = await get_message(task[1]["query_id"], logger)
+        query_id = task[1]["query_id"]
+        message = await get_message(query_id, logger)
 
         # graph retrieval
 
@@ -32,6 +45,42 @@ async def sipr(task, logger: logging.Logger):
         # prune
 
         # return results
+        # Put callback UID and query ID in postgres
+        callback_id = str(uuid.uuid4())[:8]
+        await add_callback_id(query_id, callback_id, logger)
+        # put lookup query graph in redis
+        await save_message(
+            f"{query_id}_lookup_query_graph", message["message"]["query_graph"], logger
+        )
+        message["callback"] = f"{settings.callback_host}/aragorn/callback/{callback_id}"
+
+        async with httpx.AsyncClient(timeout=100) as client:
+            await client.post(
+                settings.kg_retrieval_url,
+                json=message,
+            )
+
+        # this worker might have a timeout set for if the lookups don't finish within a certain
+        # amount of time
+        start_time = time.time()
+        running_callback_ids = [""]
+        while time.time() - start_time < MAX_QUERY_TIME:
+            # see if there are existing lookups going
+            running_callback_ids = await get_running_callbacks(query_id, logger)
+            # logger.info(f"Got back {len(running_callback_ids)} running lookups")
+            # if there are, continue to wait
+            if len(running_callback_ids) > 0:
+                await asyncio.sleep(1)
+                continue
+            # if there aren't, lookup is complete and we need to pass on to next workflow operation
+            if len(running_callback_ids) == 0:
+                logger.debug("Got all lookups back. Continuing...")
+                break
+
+        if time.time() - start_time > MAX_QUERY_TIME:
+            logger.warning(
+                f"Timed out getting lookup callbacks. {len(running_callback_ids)} queries still running..."
+            )
 
     except Exception as e:
         logger.error(f"Something bad happened! {e}")
