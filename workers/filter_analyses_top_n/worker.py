8efedd7b8aec79203598736211c605e21a0015c1
uhbrar <abrar.mesbah@gmail.com>
2026-02-10 11:01:29 -0500

    add analysis filtering

---
+++ workers/filter_analyses_top_n/worker.py
@@ -0,0 +1,65 @@
+"""Example ARA module."""
+
+import asyncio
+import json
+import logging
+import time
+import uuid
+from shepherd_utils.db import get_message, save_message, get_query_state
+from shepherd_utils.shared import get_tasks, wrap_up_task
+from shepherd_utils.otel import setup_tracer
+
+# Queue name
+STREAM = "filter_results_top_n"
+GROUP = "consumer"
+CONSUMER = str(uuid.uuid4())[:8]
+TASK_LIMIT = 100
+tracer = setup_tracer(STREAM)
+
+
+async def filter_analyses_top_n(task, logger: logging.Logger):
+    start = time.time()
+    # given a task, get the message from the db
+    response_id = task[1]["response_id"]
+    workflow = json.loads(task[1]["workflow"])
+    message = await get_message(response_id, logger)
+    results = message["message"].get("results", [])
+    current_op = workflow[0]
+    if current_op is None:
+        logger.error(f"Unable to find operation {STREAM} in workflow")
+        raise Exception(f"Operation {STREAM} is not in workflow")
+    n = current_op.get("max_analyses", 1000)
+    try:
+        for ind, result in enumerate(message["message"]["results"]):
+            message["message"]["results"][ind]["analyses"] = result["analyses"][:n]
+    except KeyError as e:
+        # can't find the right structure of message
+        logger.error(f"Error filtering results: {e}")
+        return message, 400
+    logger.info("Returning filtered results.")
+
+    # save merged message back to db
+    await save_message(response_id, message, logger)
+
+    await wrap_up_task(STREAM, GROUP, task, workflow, logger)
+    logger.info(f"Finished task {task[0]} in {time.time() - start}")
+
+
+async def process_task(task, parent_ctx, logger, limiter):
+    span = tracer.start_span(STREAM, context=parent_ctx)
+    try:
+        await filter_analyses_top_n(task, logger)
+    finally:
+        span.end()
+        limiter.release()
+
+
+async def poll_for_tasks():
+    async for task, parent_ctx, logger, limiter in get_tasks(
+        STREAM, GROUP, CONSUMER, TASK_LIMIT
+    ):
+        asyncio.create_task(process_task(task, parent_ctx, logger, limiter))
+
+
+if __name__ == "__main__":
+    asyncio.run(poll_for_tasks())
